d7d873d533a3f62659c706f2159ed3e55505c082
V HARSHA VARDHAN <v.harshavardhan3@gmail.com>
2024-10-07 14:12:03 +0530

    logout

--- src/components/childComponents/NavProfile.jsx
+++ src/components/childComponents/NavProfile.jsx
@@ -1,16 +1,17 @@
-import { MdOutlinePersonOutline } from "react-icons/md";
+import {MdOutlineLogout, MdOutlinePersonOutline} from "react-icons/md";
 
 const NavProfile = () => {
     var userName = localStorage.getItem('userName');
-
     return (
     <>
-  <div className="profile-nav-cont flex">
-      <MdOutlinePersonOutline size={27} />
-      <p className="profile-name font-bold">{userName}</p>
-    </div>
+        <div className="profile-nav-cont flex">
+            <MdOutlinePersonOutline size={27}/>
+            <p className="profile-name font-bold mx-4">{userName}</p>
+            <MdOutlineLogout size={27}/>
+            <button className="profile-name font-bold">Logout</button>
+        </div>
     </>
-  )
+    )
 }
 
 export default NavProfile
