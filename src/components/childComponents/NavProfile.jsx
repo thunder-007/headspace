7223b62d7664ff9ba00079e0af55dc6cf36fc153
V HARSHA VARDHAN <v.harshavardhan3@gmail.com>
2024-10-07 14:19:31 +0530

    logout functionality update

--- src/components/childComponents/NavProfile.jsx
+++ src/components/childComponents/NavProfile.jsx
@@ -1,14 +1,20 @@
 import {MdOutlineLogout, MdOutlinePersonOutline} from "react-icons/md";
-
+import {logout} from "../../../firebase-config.js";
 const NavProfile = () => {
+    const handleLogout = async () => {
+        await logout();
+        window.location.href = "/";
+    };
+
     var userName = localStorage.getItem('userName');
     return (
     <>
         <div className="profile-nav-cont flex">
             <MdOutlinePersonOutline size={27}/>
             <p className="profile-name font-bold mx-4">{userName}</p>
+
             <MdOutlineLogout size={27}/>
-            <button className="profile-name font-bold">Logout</button>
+            <button onClick={handleLogout} className="profile-name font-bold">Logout</button>
         </div>
     </>
     )
